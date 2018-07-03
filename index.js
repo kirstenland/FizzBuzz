3eb0ed9b3886bef4ee29c97849a22974f6a0c93a
Kirsten Land <Kirsten.Land@softwire.com>
2018-07-03 15:54:45 +0100

    Added Bang for multiples of 7.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 for (let i = 1; i <= 100; i++) {
-    if (i % 15 == 0) {
-        console.log("FizzBuzz");
-    } else if (i % 3 == 0) {
-        console.log("Fizz");
-    } else if (i % 5 == 0) {
-        console.log("Buzz");
-    } else {
+    var modifiers = new Array();
+    if (i % 3 == 0) {
+        modifiers.push("Fizz");
+    }
+    if (i % 5 == 0) {
+        modifiers.push("Buzz");
+    }
+    if (i % 7 == 0) {
+        modifiers.push("Bang");
+    }
+    if (modifiers.length == 0) {
         console.log(i);
+    } else {
+        console.log(modifiers.join(deliminator=""));
     }
 }
