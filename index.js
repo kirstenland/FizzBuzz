99f017d376932f0e4cf95be8e44a9b76e7f5923f
Kirsten Land <Kirsten.Land@softwire.com>
2018-07-03 17:55:30 +0100

    Changed variable names

--- index.js
+++ index.js
@@ -3,48 +3,45 @@ for (let i = 1; i <= 100; i++) {
 }
 
 function evaluateNumber(i) {
-    let modifiers = [];
-    const rules = [{number: 3, rule: adderToModifiers("Fizz")},
-                   {number: 5, rule: adderToModifiers("Buzz")},
-                   {number:7, rule: adderToModifiers("Bang")},
+    let words = [];
+    const rules = [{number: 3, rule: (words) => addToWords("Fizz", words)},
+                   {number: 5, rule: (words) => addToWords("Buzz", words)},
+                   {number:7, rule: (words) => addToWords("Bang", words)},
                    {number:11, rule: processBong},
                    {number:13, rule: processFezz},
                    {number:17, rule: processReverse}];
     for(let j = 0; j < rules.length; j++) {
         let nextRule = rules[j];
         if (i % nextRule.number == 0) {
-            modifiers = nextRule.rule(modifiers);
+            words = nextRule.rule(words);
         }
     }
-    if (modifiers.length == 0) {
+    if (words.length == 0) {
         return i;
     } else {
-        return modifiers.join(deliminator="");
+        return words.join(deliminator="");
     }
 }
 
-function adderToModifiers(word) {
-    function addToModifiers(modifiers) {
-        modifiers.push(word);
-        return modifiers;
-    }
-    return addToModifiers;
+function addToWords(word, words) {
+    words.push(word);
+    return words;
 }
 
-function processBong(modifiers) {
+function processBong(words) {
     return ["Bong"];
 }
 
-function processFezz(modifiers) {
+function processFezz(words) {
     for (let i = 0;; i++ ) {
-        if (i >= modifiers.length || modifiers[i][0] == "B") {
-            modifiers.splice(i, 0, "Fezz");
-            return modifiers;
+        if (i >= words.length || words[i][0] == "B") {
+            words.splice(i, 0, "Fezz");
+            return words;
         }
     }
 }
 
-function processReverse(modifiers) {
-    modifiers.reverse();
-    return modifiers;
+function processReverse(words) {
+    words.reverse();
+    return words;
 }
