a6262b92818f5ab012941d988be3e99b5a261b1c
Kirsten Land <Kirsten.Land@softwire.com>
2018-07-03 17:37:50 +0100

    Refactored to go through a list of numbers and rules

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-
 for (let i = 1; i <= 100; i++) {
     console.log(evaluateNumber(i));
 }
 
 function evaluateNumber(i) {
     let modifiers = [];
-    if (i % 3 == 0) {
-        modifiers.push("Fizz");
-    }
-    if (i % 5 == 0) {
-        modifiers.push("Buzz");
-    }
-    if (i % 7 == 0) {
-        modifiers.push("Bang");
-    }
-    if (i % 11 == 0) {
-        modifiers = ["Bong"];
-    }
-    if (i % 13 == 0) {
-        modifiers = insertFezz(modifiers);
-    }
-    if (i % 17 == 0) {
-        modifiers.reverse();
+    const rules = [{number: 3, rule: adderToModifiers("Fizz")},
+                   {number: 5, rule: adderToModifiers("Buzz")},
+                   {number:7, rule: adderToModifiers("Bang")},
+                   {number:11, rule: processBong},
+                   {number:13, rule: processFezz},
+                   {number:17, rule: processReverse}];
+    for(let j = 0; j < rules.length; j++) {
+        let nextRule = rules[j];
+        if (i % nextRule.number == 0) {
+            modifiers = nextRule.rule(modifiers);
+        }
     }
     if (modifiers.length == 0) {
         return i;
@@ -30,11 +23,28 @@ function evaluateNumber(i) {
     }
 }
 
-function insertFezz(modifiers) {
+function adderToModifiers(word) {
+    function addToModifiers(modifiers) {
+        modifiers.push(word);
+        return modifiers;
+    }
+    return addToModifiers;
+}
+
+function processBong(modifiers) {
+    return ["Bong"];
+}
+
+function processFezz(modifiers) {
     for (let i = 0;; i++ ) {
         if (i >= modifiers.length || modifiers[i][0] == "B") {
             modifiers.splice(i, 0, "Fezz");
             return modifiers;
         }
     }
+}
+
+function processReverse(modifiers) {
+    modifiers.reverse();
+    return modifiers;
 }
